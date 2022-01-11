64913f7d29740d23f630ee7a15a258505da747ab
Alexander Bazo <alexanderbazo@googlemail.com>
2022-01-11 17:33:26 +0100

    Add comments

--- src/CirclePainterDeluxe.java
+++ src/CirclePainterDeluxe.java
@@ -9,17 +9,22 @@ import de.ur.mi.oop.utils.DebugInfo;
 
 import java.util.ArrayList;
 
+/**
+ * In diesem Beispiel wird eine Liste sich bewegender Kreise verwaltet. Bei Mausklicks werden an der
+ * Position des Mauszeigers neue Kreise erstellt, die im Rahmen des Draw-Loops bewegt und gezeichnet werden.
+ * Verlässt ein Kreis dabei die Zeichenfläche, wird er entfernt.
+ */
 public class CirclePainterDeluxe extends GraphicsApp implements MovingCircle.MovingCircleListener {
 
-    private ArrayList<MovingCircle> circles;
-    private ArrayList<MovingCircle> circlesToBeRemoved;
-    private Label infoLabel;
+    private ArrayList<MovingCircle> circles; // Liste aller darzustellenden Kreise
+    private ArrayList<MovingCircle> circlesToBeRemoved; // Liste der Kreise, die vor dem nächsten Zeichnen gelöscht werden sollen
+    private Label infoLabel; // Label, in dem die aktuelle Anzahl an sichtbaren Kreisen angezeigt wird
 
     @Override
     public void initialize() {
         setCanvasSize((int) Settings.WINDOW_WIDTH, (int) Settings.WINDOW_HEIGHT);
-        circles = new ArrayList<>();
-        circlesToBeRemoved = new ArrayList<>();
+        circles = new ArrayList<>(); // Erstellen einer neuen, leeren ArrayList für die Kreise
+        circlesToBeRemoved = new ArrayList<>(); // Erstellen einer neuen, leeren ArrayList für die zu löschenden Kreise
         infoLabel = new Label(24, 24, "", Theme.RED);
         infoLabel.setFont("Arial Rounded MT Bold");
         infoLabel.setFontSize(14);
@@ -28,11 +33,11 @@ public class CirclePainterDeluxe extends GraphicsApp implements MovingCircle.Mov
     @Override
     public void draw() {
         drawBackground(Theme.GREY);
-        updateCircles();
-        clearCircles();
-        drawCircles();
-        infoLabel.draw();
+        updateCircles(); // Alle Kreise werden bewegt (Falls ein Kreis dabei den Bildschirm verlässt, erfahren wir das in "hasLeftScene")
+        clearCircles(); // Entfernen aller Kreise, die durch updateCircles den Bildschirm verlassen haben
+        drawCircles(); // Zeichnen aller verbleibenden Kreise
         infoLabel.setText("circles in scene: " + circles.size());
+        infoLabel.draw();
     }
 
     private void updateCircles() {
@@ -42,6 +47,11 @@ public class CirclePainterDeluxe extends GraphicsApp implements MovingCircle.Mov
     }
 
     private void clearCircles() {
+        /*
+         * Hier machen wir vollen Gebrauch von den sehr einfachen Methoden der ArrayList:
+         * - Wir entfernen alle Kreise aus circles, die auch in circlesToBeRemoved gespeichert sind
+         * - Im Anschluss leeren wir die circleToBeRemoved-Liste, damit wird im nächsten Frame hier erneut die zu löschenden Kreise sammeln können
+         */
         circles.removeAll(circlesToBeRemoved);
         circlesToBeRemoved.clear();
     }
@@ -55,11 +65,25 @@ public class CirclePainterDeluxe extends GraphicsApp implements MovingCircle.Mov
 
     @Override
     public void onMousePressed(MousePressedEvent event) {
+        /*
+         * Beim Klick mit der Maus wird ein neuer Kreis an der Position des Mauszeigers erstellt und in der ArrayList gespeichert.
+         * Dabei gibt sich die GraphicsApp-Klasse als Listener aus (deshalb implementieren wir in dieser Klasse auch
+         * das Interface MovingCircle.MovingCircleListener).
+         */
         circles.add(new MovingCircle(event.getXPos(), event.getYPos(), this));
     }
 
     @Override
     public void hasLeftScene(MovingCircle circle) {
+        /*
+         * Wenn uns ein Kreis mitteil, dass er den Bildschirm verlassen hat, merken wir uns das, in dem wir den Kreis in einer
+         * separaten Liste speichern. Das müssen wir deshalb machen, da wir nicht gleichzeitig über eine ArrayList iterieren
+         * können (siehe udpateCircles) und Inhalte aus dieser entfernen können. Der Aufruf dieser Methode aus einer der
+         * MovingCircle-Instanzen kann nur aus der for-Schleife in updateCircles ausgelöst werden, in der für jeden Kreis
+         * die update-Methode ausgelöst wird. Statt den Kreis direkt zu löschen - was einen Fehler verursachen würde - merken
+         * wir uns alle zu löschenden Kreise und entfernen diese erst, wenn die Iteration über die Liste abgeschlossen ist. Das
+         * passiert dann nach dem Ende der Methode "updateCircles" in einer separaten Methode "clearCircles".
+         */
         circlesToBeRemoved.add(circle);
     }
 
--- src/MovingCircle.java
+++ src/MovingCircle.java
@@ -1,17 +1,33 @@
 import config.Theme;
 import de.ur.mi.oop.graphics.Circle;
 
+/**
+ * Diese Klasse repräsentiert einen Kreis, der sich mit zufälliger Geschwindigkeit über
+ * die Zeichenfläche bewegt. Über eine einfache Listener-Schnittstelle informiert der
+ * Kreis selbstständig einen registrierten Listener (Observer), sobald er sich durch
+ * die regelmäßig stattfindende Bewegung (siehe update-Methode) vollständig aus dem sichtbaren
+ * Bereich der Zeichenfläche entfernt hat.
+ */
 public class MovingCircle {
 
-    private final float CIRCLE_RADIUS = 20;
-    private final float MIN_SPEED = 1;
-    private final float MAX_SPEED = 4;
+    private final float CIRCLE_RADIUS = 20; // Radius der Kreise
+    private final float MIN_SPEED = 1; // Minimale Geschwindigkeit (absoluter Wert)
+    private final float MAX_SPEED = 4; // Maximale Geschwindigkeit (absoluter Wert)
 
-    private final Circle shape;
-    private final float xVelocity;
-    private final float yVelocity;
-    private final MovingCircleListener listener;
+    private final Circle shape; // Circle zur Darstellung des Kreises
+    private final float xVelocity; // Geschwindigkeit des Kreises auf der X-Achse (Pixel per Frame)
+    private final float yVelocity; // Geschwindigkeit des Kreises auf der Y-Achse (Pixel per Frame)
+    private final MovingCircleListener listener; // Listener, der informiert werden soll, wenn Kreis Zeichenfläche verlässt
 
+    /**
+     * Konstruktor für neue "Moving Circles". Als Parameter wird die Bildschirmposition übergeben,
+     * an der der neue Kreis initial angezeigt werden soll. Zusätzlich wird ein Listener übergeben,
+     * der vom Kreis informiert wird, sobald sich dieser später aus dem Bildschirm heraus bewegt. Alle
+     * weiteren Eigenschaften werden zufällige im Konstruktor bestimmt.
+     * @param x Initiale Position des Kreises auf der X-Achse
+     * @param y Initiale Position des Kreises auf der Y-Achse
+     * @param listener Listener, der über das Verlassen der Zeichenfläche informiert werden soll
+     */
     public MovingCircle(float x, float y, MovingCircleListener listener) {
         this.shape = new Circle(x, y, CIRCLE_RADIUS, Theme.YELLOW);
         this.xVelocity = createRandomVelocity();
@@ -19,6 +35,13 @@ public class MovingCircle {
         this.listener = listener;
     }
 
+    /**
+     * Diese Methode gibt eine zufällige Geschwindigkeit für die Bewegung des Kreises auf einer der Achsen zurück.
+     * Dabei wird zuerst ein zufälliger, absoluter Wert, auf Basis der Grenzwerte in MIN_SPEED und MAX_SPEED bestimmt.
+     * Im Anschluss wird, ebenfalls zufällig, die Bewegungsrichtung festgelegt, in dem der absolute Wert mit einer
+     * Wahrscheinlichkeit von p = 0.5 mit einem negativen Vorzeichen versehen wird.
+     * @return Die zufällig bestimmte Geschwindigkeit (inkl. Bewegungsrichtung)
+     */
     private float createRandomVelocity() {
         float v = (float) (MIN_SPEED + Math.random() * (MAX_SPEED - MIN_SPEED));
         if(Math.random() > 0.5) {
@@ -27,10 +50,22 @@ public class MovingCircle {
         return v;
     }
 
+    /**
+     * Diese Methode bewegt den Kreis auf Basis der im Konstruktor festgelegten Geschwindigkeiten. Falls der Kreis
+     * nach der Bewegung die sichtbare Zeichenfläche verlassen hat, wird der im Konstruktor gespeicherte Listener
+     * informiert. Grundlage für diese Prüfung sind die Dimensionen der Zeichenfläche, die als Parameter an diese
+     * Methode übergeben werden.
+     * @param sceneWidth Breite der Zeichenfläche in Pixel, auf der der Kreis sich bewegt
+     * @param sceneHeight Höhe der Zeichenfläche in Pixel, auf der der Kreis sich bewegt
+     */
     public void update(float sceneWidth, float sceneHeight) {
         shape.move(xVelocity, yVelocity);
+        /*
+         * Hier prüfen wir anhand der jeweiligen Grenzbereiche des Kreises (Boundingbox), ob dieser sich noch
+         * vollständig in der Zeichenfläche befindet, die über die übergebenen Parameter definiert wird.
+         */
         if(shape.getLeftBorder() > sceneWidth || shape.getTopBorder() > sceneHeight || shape.getRightBorder() < 0 || shape.getBottomBorder() < 0) {
-            listener.hasLeftScene(this);
+            listener.hasLeftScene(this); // Kommunikation mit dem Listener, falls Kreis die Zeichenfläche verlassen haben sollte
         }
     }
 
@@ -38,6 +73,13 @@ public class MovingCircle {
         shape.draw();
     }
 
+    /**
+     * Interface, mit dem sich der Listener gegenüber dem MovingCircle ausgibt. Klassen und Interfaces können, wie hier,
+     * auch als (öffentlicher) Teil einer bereits bestehenden Klasse definiert werden. Häufig wird dies, statt der Definition
+     * in einer separaten Datei, verwendet, wenn es sich um sehr übersichtliche Interfaces handelt, die in einer direkten
+     * Beziehung zur umschließenden Klasse stehen. Das ist hier auch der Fall: Das Interface definiert nur eine Methode und bezieht
+     * sich explizit auf die Klasse MovingCircle.
+     */
     public interface MovingCircleListener {
 
         void hasLeftScene(MovingCircle circle);
